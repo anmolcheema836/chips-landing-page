59097a2581951b87335f469bc81cc4e54ec2dafe
anmolcheema836 <g18561128@gmail.com>
2024-11-05 13:08:28 +0530

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../Stylesheets/Footer.css"
+export default function Footer() {
+  return (
+    <footer className='Footer'>
+        <div className='FooterContainer'>
+
+        </div>
+        <div className='copyrightSection'>
+        <p>Cheema Chips &#169; Copyright 2024 | All rights reserved</p>
+        </div>
+    </footer>
+  )
+}
